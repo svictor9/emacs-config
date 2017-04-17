eabba782f37e2317b954bc5e506196c7c1839e97
Victor A. Stoichiţă <victor@svictor.net>
2017-04-18 00:53:34 +0200

    Theme corrections. Switch to sanityinc

--- init.el
+++ init.el
@@ -9,9 +9,7 @@
 			    ("org" . "http://orgmode.org/elpa/")))
 
 ;; Theme
-   ;; (require 'moe-theme)
-   ;; (moe-dark)
-   (color-theme-sanityinc-tomorrow-bright)
+   (load-theme 'sanityinc-tomorrow-night t)
 
 
 ;; Helm mode
@@ -567,10 +565,6 @@
  '(ansi-color-names-vector
    (vector "#eaeaea" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#000000"))
  '(backup-directory-alist (quote ((".*bufni.*" . "/tmp"))))
- '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
- '(custom-safe-themes
-   (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "c4e6fe8f5728a5d5fd0e92538f68c3b4e8b218bcfb5e07d8afff8731cc5f3df0" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
  '(delete-selection-mode t)
  '(dired-dwim-target t)
  '(eww-search-prefix "https://www.google.com/search?q=")

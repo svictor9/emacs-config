af4bc3a2fdd1d5e17308b1bb0fbcbd763f3a45e0
Victor A. Stoichiţă <victor@svictor.net>
2018-01-03 18:14:35 +0100

    ajouté fichiers restants à gitignore. Enlevé second setting de color theme (?)

--- init.el
+++ init.el
@@ -628,7 +628,6 @@
  '(auto-save-list-file-prefix "~/.emacs.d/.autogeneres/auto-save-list/saves-")
  '(backup-directory-alist (quote ((".*" . "/home/vic/.emacs.d/backups/"))))
  '(beacon-color "#f2777a")
- '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
  '(custom-safe-themes
    (quote
     ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
